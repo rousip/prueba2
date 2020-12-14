edf0d17b6277b3cf3aa5acaa66320b8f08e92077
ivan-endesa <ivan.delossantos@enel.com>
2020-12-14 17:51:02 +0100

    Añadir importe

--- suma_importes.py
+++ suma_importes.py
@@ -1,3 +1,2 @@
-def calcular_suma_importes(importe_1, importe_2, importe_3):
-    
-    return importe_1+importe_2+importe_3
+def calcular_suma_importes(importe_1, importe_2, importe_3, num_dias):
+    return (importe_1+importe_2+importe_3) / num_dias

a5434a67162b4db573ec5603af20213f2a58bc1d
ivan-endesa <ivan.delossantos@enel.com>
2020-12-14 17:55:09 +0100

    Funcion impuestos

--- suma_importes.py
+++ suma_importes.py
@@ -1,2 +1,2 @@
-def calcular_suma_importes(importe_1, importe_2, importe_3, num_dias):
-    return (importe_1+importe_2+importe_3) / num_dias
+def calcular_suma_importes(importe_1, importe_2, importe_3, num_dias, impuestos):
+    return ((importe_1+importe_2+importe_3) / num_dias) * impuestos

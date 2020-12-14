88b7295ce236d95c7e6a3961251b75d05601660f
Rosa Cabeza Gavira <34445612+rousip@users.noreply.github.com>
2020-12-14 18:04:33 +0100

    Añadimos impuesto por defecto

--- suma_importes.py
+++ suma_importes.py
@@ -1,2 +1,2 @@
-def calcular_suma_importes(importe_1, importe_2, importe_3, num_dias, impuestos):
+def calcular_suma_importes(importe_1, importe_2, importe_3, num_dias, impuestos=0.21):
     return ((importe_1+importe_2+importe_3) / num_dias) * impuestos
